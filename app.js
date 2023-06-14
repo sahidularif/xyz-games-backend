9c5bb75f183e5ebfa8419810d83efcc4f8e05cbc
sahidularif <sahidularif1@gmail.com>
2023-06-15 01:49:21 +0600

    update app.js

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const express = require("express")
-const app = express(); 
-const mongoose = require("mongoose")
-const db = require('./config/keys_development').mongoURI
-const users = require("./routes/api/users")
-const comments = require('./routes/api/comments')
-const games = require("./routes/api/games")
-const User = require("./models/User")
-const bodyParser = require("body-parser")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const db = require('./config/keys_development').mongoURI;
+const users = require("./routes/api/users");
+const comments = require('./routes/api/comments');
+const games = require("./routes/api/games");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
 const passport = require("passport");
-const getGameOdds = require('./util/odds')
-const getGameResults = require('./util/games')
-const Game = require('./models/Game')
-const bets = require('./routes/api/bets')
-const path = require('path')
-const resolveBets = require('./util/resolveBets')
-const lockBets = require('./util/lockBets')
+const getGameOdds = require('./util/odds');
+const getGameResults = require('./util/games');
+const Game = require('./models/Game');
+const bets = require('./routes/api/bets');
+const path = require('path');
+const resolveBets = require('./util/resolveBets');
+const lockBets = require('./util/lockBets');
 const router = require('express').Router();
-const cors = require("cors")
+const cors = require("cors");
 require('dotenv').config();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({
+  credentials: true,
+  origin: "*",
+}));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
@@ -26,7 +32,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
-mongoose.set('strictQuery', false)
+
+mongoose.set('strictQuery', false);
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
@@ -35,38 +43,25 @@ const connectDB = async () => {
     console.log(error);
     process.exit(1);
   }
-}
-
-app.use(bodyParser.urlencoded({
-  extended: true,
-}) )
-
-app.use(bodyParser.json()); 
-app.use(cors({
-  credentials: true,
-  origin: "*",
-}));
+};
 
 // Routes
 app.use("/api/users", users);
-app.use("/api/games", games)
-app.use("/api/bets", bets)
-app.use("/api/comments", comments)
-
-
-app.get("/", (req, res) => res.send("Hello World!!"));
+app.use("/api/games", games);
+app.use("/api/bets", bets);
+app.use("/api/comments", comments);
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
-// resolveBets("61b27bde4e8f1d6815de12ab", "Lakers");
+app.get("/", (req, res) => res.send("Hello World!!"));
 
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
 connectDB().then(() => {
   app.listen(port, () => {
-      console.log(`Listening on port: ${port}`);
-  })
-})
+    console.log(`Listening on port: ${port}`);
+  });
+});
 
 getGameResults();
